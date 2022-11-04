56a65fc60c1ac04a8a80dc9d65bb0749a75d0c1f
AlphaGodzilla <AlphaGodzilla@protonmail.com>
2022-11-05 02:45:40 +0800

    add action configure

--- asia.js
+++ asia.js
@@ -2,7 +2,7 @@ const { scheduleForCheckIn } = require('./code')
 
 // const token = '5c38941a0a1b462596cc63108fc0b345'
 const token = process.argv[3]
-console.log('token', token)
+// console.log('token', token)
 
 // asia check in
 scheduleForCheckIn(token, 'asia')
